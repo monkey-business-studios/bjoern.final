d60f52095c2e98e1b9e8205acea18da0e82a6b1a
eddie-github <edgar.schoenknecht@hotmail.de>
2016-08-01 11:49:04 +0200

    Project added

---
+++ Assets/Scripts/CharacterCombat.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCombat : MonoBehaviour {
+
+    public float clawSpeed = 600.0f;
+    public Transform clawSpawn; // Der Ort wo der "Schuss/Klaue" spawnt
+    public Rigidbody clawPrefab;
+
+    Rigidbody clone;
+
+    void Awake()
+    {
+        clawSpawn = GameObject.Find("ClawSpawn").transform; // Der Variable wird das entsprechende GameObject zugewiesen
+    }
+	
+	void Start ()
+    {
+	
+	}
+	
+
+	void Update ()
+    {
+        // Attack MouseButton1
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+	}
+
+    void Attack()
+    {
+        clone = Instantiate(clawPrefab, clawSpawn.position, clawSpawn.rotation) as Rigidbody;
+        clone.AddForce(clawSpawn.transform.right * clawSpeed);
+    }
+}
